532997854e919a234581d352353e33f57d2b8576
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-13 15:29:59 +0530

    Update load_fdb_daily.sh

--- src/main/resources/load_fdb_daily.sh
+++ src/main/resources/load_fdb_daily.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# Set the necessary environment variables
-export FDB_HOME=/mnt/c/Users/AMBATS/Desktop/Batchjob/FDBDailyPrice
-export DATA_DIR=/mnt/c/Users/AMBATS/Desktop/Batchjob/Destinationfile
-# Change to the FDB home directory
-cd "$FDB_HOME"
-# Run the FDB import command
-./fdb_import -d "$DATA_DIR/FDB_Daily_price_updates.csv"
+
+echo "Script is start"
+
+#awk -F',' '{print $0}' FDB_Daily_price_updatesnew.csv
+
+source FDB_Daily_price_updatesnew.csv
+
+echo "Script is running"
