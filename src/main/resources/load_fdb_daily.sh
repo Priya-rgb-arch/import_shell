f8cf2380100d624898f60e98c46684ee4b808cb2
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-13 15:32:23 +0530

    Update load_fdb_daily.sh

--- src/main/resources/load_fdb_daily.sh
+++ src/main/resources/load_fdb_daily.sh
@@ -2,8 +2,8 @@
 
 echo "Script is start"
 
-#awk -F',' '{print $0}' FDB_Daily_price_updatesnew.csv
+awk -F',' '{print $0}' FDB_Daily_price_updatesnew.csv
 
-source FDB_Daily_price_updatesnew.csv
+#source FDB_Daily_price_updatesnew.csv
 
 echo "Script is running"
